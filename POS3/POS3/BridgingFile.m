d1d878a0c6392ad497bb03f30c362249d93dcb1b
King Him Cheung <kingcheung0@gmail.com>
2017-09-21 19:48:47 +0100

    deleted

--- POS3/POS3/BridgingFile.m
+++
@@ -1,8 +0,0 @@
-//
-//  BridgingFile.m
-//  POS3
-//
-//  Created by King Cheung on 27/08/2017.
-//  Copyright © 2017 King Cheung. All rights reserved.
-//
-
--- POS3/POS3/bridgingHeader.m
+++
@@ -1,9 +0,0 @@
-//
-//  bridgingHeader.m
-//  POS3
-//
-//  Created by King Cheung on 27/08/2017.
-//  Copyright © 2017 King Cheung. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
